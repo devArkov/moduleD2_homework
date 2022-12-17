3a0cdc5912dde7e962ff53d1ab5d070c97f56888
Alexandr Korobov <devarko@sapo.pt>
2022-12-17 19:51:04 +0300

    Render view + about, contact pages & navbar

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', views.home, name='home'),
 ]
